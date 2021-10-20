904a58202f7d8d4d8f81f2dfff9400632b590089
Jeongs <kimm.jeongs@gmail.com>
2021-10-20 21:31:35 +0900

    Feat: 회원가입 컴포넌트 라우팅 설정

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import Signup from '~/routes/Signup'
 
 export default createRouter({
-	history: createWebHistory(),
-	scrollBehavior: () => ({ top: 0 }),
-	routes: [],
+  history: createWebHistory(),
+  scrollBehavior: () => ({ top: 0 }),
+  routes: [
+    {
+      name: 'Signup',
+      path: '/signup',
+      component: Signup,
+    },
+  ],
 })
