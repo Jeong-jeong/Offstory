986fece94ea17079177c1474d057fe23571bd9d0
wherehows <sensitive.hu@gmail.com>
2021-10-21 23:01:44 +0900

    Feat: Header 문자열 파싱 로직 구현

--- src/routes/Signup.vue
+++ src/routes/Signup.vue
@@ -1,50 +1,50 @@
 <template>
-  <div class="sign-page">
-    <div class="container">
-      <Deemed />
-      <div class="row">
-        <div class="col-lg-8">
-          <form class="sign-form" @submit.prevent="submitSignup">
-            <input v-model="email" type="email" required />
-            <input ref="pw" v-model="password" type="password" required />
-            <input
-              ref="checkPw"
-              type="password"
-              class="checkPassword"
-              required
-            />
-            <input v-model="fullName" type="text" class="name" required />
+	<div class="sign-page">
+		<div class="container">
+			<Deemed />
+			<div class="row">
+				<div class="col-lg-8">
+					<form class="sign-form" @submit.prevent="submitSignup">
+						<input v-model="email" type="email" required />
+						<input ref="pw" v-model="password" type="password" required />
+						<input
+							ref="checkPw"
+							type="password"
+							class="checkPassword"
+							required
+						/>
+						<input v-model="fullName" type="text" class="name" required />
 
-            <button>회원가입</button>
-          </form>
-        </div>
-      </div>
-    </div>
-  </div>
+						<button>회원가입</button>
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
 import requestSignup from '~/api/requestSignup'
 
 export default {
-  data() {
-    return {
-      email: '',
-      password: '',
-      fullName: '',
-    }
-  },
+	data() {
+		return {
+			email: '',
+			password: '',
+			fullName: '',
+		}
+	},
 
-  methods: {
-    async submitSignup() {
-      const res = await requestSignup({
-        email: this.email,
-        password: this.password,
-        fullName: this.fullName,
-      })
-      console.log(res)
-    },
-  },
+	methods: {
+		async submitSignup() {
+			const res = await requestSignup({
+				email: this.email,
+				password: this.password,
+				fullName: this.fullName,
+			})
+			console.log(res)
+		},
+	},
 }
 </script>
 
